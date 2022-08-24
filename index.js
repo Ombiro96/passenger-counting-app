23932dffd36853b055214ee795a4cf7f7fda13b4
ombiro96 <bakutarb@gmail.com>
2022-08-24 21:08:51 +0300

    checkpoint

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 //document.getElementById("count-el").innerText=5
-// let count=0
+let count=0
 
-// let countEl = document.getElementById("count-el")
+let countEl = document.getElementById("count-el")
 // //the DOM/document object model... how you use javaScript to modify a website
 // //document... object
 // //model 
 
-// console.log(countEl)
+ console.log(countEl)
 
-// function increment(){
-//     count=count+1
-//     countEl.innerText=count
-//     console.log(count)
-// }
-// function save(){
-//     console.log("Previous entries: ", + count)
-// }
-let name=47
-let greeting="Hi, my name is "
+ function increment(){
+   count=count+1
+    countEl.innerText=count
+    console.log(count)
+ }
+ function save(){
+     console.log("Previous entries: ", + count)
+ }
+let welcomeEl=document.getElementById("welcome-el")
+let name="Clifford Ombiro Mwenda"
+let message="Welcome back, "
 
-let myGreeting=greeting + name
-
-console.log(myGreeting)
-
-console.log(4+5)//9
-console.log("2"+"4")//"24"
-console.log("5"+1)//"51"
-console.log(100+"100")//"100100"
+welcomeEl.innerText= message + name
