a6b7dfca68e1387b5ba6bb49c7471e0bdfa4defb
ombiro96 <bakutarb@gmail.com>
2022-08-24 20:03:37 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+//document.getElementById("count-el").innerText=5
+// let count=0
+
+// let countEl = document.getElementById("count-el")
+// //the DOM/document object model... how you use javaScript to modify a website
+// //document... object
+// //model 
+
+// console.log(countEl)
+
+// function increment(){
+//     count=count+1
+//     countEl.innerText=count
+//     console.log(count)
+// }
+// function save(){
+//     console.log("Previous entries: ", + count)
+// }
+let name=47
+let greeting="Hi, my name is "
+
+let myGreeting=greeting + name
+
+console.log(myGreeting)
+
+console.log(4+5)//9
+console.log("2"+"4")//"24"
+console.log("5"+1)//"51"
+console.log(100+"100")//"100100"
